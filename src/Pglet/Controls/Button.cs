ecbe5e9b7e93487de3363ac1802d56169b0b3ef0
Feodor Fitsner <feodor@appveyor.com>
2021-03-22 21:12:38 -0700

    Button props

--- src/Pglet/Controls/Button.cs
+++ src/Pglet/Controls/Button.cs
@@ -6,10 +6,82 @@ namespace Pglet.Controls
 {
     public class Button : Control
     {
+        public bool Primary
+        {
+            get { return GetBoolAttr("primary"); }
+            set { SetBoolAttr("primary", value); }
+        }
+
+        public bool Compound
+        {
+            get { return GetBoolAttr("compound"); }
+            set { SetBoolAttr("compound", value); }
+        }
+
+        public bool Action
+        {
+            get { return GetBoolAttr("action"); }
+            set { SetBoolAttr("action", value); }
+        }
+
+        public bool Toolbar
+        {
+            get { return GetBoolAttr("toolbar"); }
+            set { SetBoolAttr("toolbar", value); }
+        }
+
+        public bool Split
+        {
+            get { return GetBoolAttr("split"); }
+            set { SetBoolAttr("split", value); }
+        }
+
         public string Text
         {
             get { return GetAttr("text"); }
             set { SetAttr("text", value); }
+        }
+
+        public string SecondaryText
+        {
+            get { return GetAttr("secondaryText"); }
+            set { SetAttr("secondaryText", value); }
+        }
+
+        public string Url
+        {
+            get { return GetAttr("url"); }
+            set { SetAttr("url", value); }
+        }
+
+        public bool NewWindow
+        {
+            get { return GetBoolAttr("newWindow"); }
+            set { SetBoolAttr("newWindow", value); }
+        }
+
+        public string Title
+        {
+            get { return GetAttr("title"); }
+            set { SetAttr("title", value); }
+        }
+
+        public string Icon
+        {
+            get { return GetAttr("icon"); }
+            set { SetAttr("icon", value); }
+        }
+
+        public string IconColor
+        {
+            get { return GetAttr("iconColor"); }
+            set { SetAttr("iconColor", value); }
+        }
+
+        public string Data
+        {
+            get { return GetAttr("data"); }
+            set { SetAttr("data", value); }
         }
 
         public EventHandler OnClick
