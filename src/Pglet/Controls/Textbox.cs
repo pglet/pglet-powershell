f4c990816ad213a2ad2289b397a84e2aa3b60139
Feodor Fitsner <feodor@appveyor.com>
2021-03-30 21:37:35 -0700

    Update Textbox.cs

--- src/Pglet/Controls/Textbox.cs
+++ src/Pglet/Controls/Textbox.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Pglet.Controls
 {
+    public enum TextBoxAlign
+    {
+        [Description("left")]
+        Left,
+
+        [Description("right")]
+        Right
+    }
+
     public class TextBox : Control
     {
         protected override string ControlName => "textbox";
@@ -20,10 +30,46 @@ namespace Pglet.Controls
             set { SetAttr("value", value); }
         }
 
+        public string Placeholder
+        {
+            get { return GetAttr("placeholder"); }
+            set { SetAttr("placeholder", value); }
+        }
+
+        public string ErrorMessage
+        {
+            get { return GetAttr("errorMessage"); }
+            set { SetAttr("errorMessage", value); }
+        }
+
+        public string Description
+        {
+            get { return GetAttr("description"); }
+            set { SetAttr("description", value); }
+        }
+
+        public TextBoxAlign Align
+        {
+            get { return GetEnumAttr<TextBoxAlign>("align"); }
+            set { SetEnumAttr("align", value); }
+        }
+
         public bool Multiline
         {
             get { return GetBoolAttr("multiline"); }
             set { SetBoolAttr("multiline", value); }
+        }
+
+        public bool Password
+        {
+            get { return GetBoolAttr("password"); }
+            set { SetBoolAttr("password", value); }
+        }
+
+        public bool Required
+        {
+            get { return GetBoolAttr("required"); }
+            set { SetBoolAttr("required", value); }
         }
 
         public EventHandler OnChange
