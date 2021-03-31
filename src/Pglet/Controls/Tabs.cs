dcd7701e96eecf12e078f82ddf0583c58121fb75
Feodor Fitsner <feodor@appveyor.com>
2021-03-31 09:47:21 -0700

    Tabs

---
+++ src/Pglet/Controls/Tabs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pglet.Controls
+{
+    public partial class Tabs : Control
+    {
+        protected override string ControlName => "tabs";
+
+        IList<Tab> _tabs = new List<Tab>();
+        public IList<Tab> TabItems
+        {
+            get { return _tabs; }
+            set { _tabs = value; }
+        }
+
+        public string Value
+        {
+            get { return GetAttr("value"); }
+            set { SetAttr("value", value); }
+        }
+
+        public bool Solid
+        {
+            get { return GetBoolAttr("solid"); }
+            set { SetBoolAttr("solid", value); }
+        }
+
+        public EventHandler OnChange
+        {
+            get { return GetEventHandler("change"); }
+            set { SetEventHandler("change", value); }
+        }
+
+        protected override IEnumerable<Control> GetChildren()
+        {
+            return _tabs;
+        }
+    }
+}
