68a427e996d450507304316c4f7581aecd0ed911
Feodor Fitsner <feodor@appveyor.com>
2021-03-31 09:36:38 -0700

    Slider, SpinButton

---
+++ src/Pglet/Controls/Slider.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Pglet.Controls
+{
+    public class Slider : Control
+    {
+        protected override string ControlName => "slider";
+        
+        public string Label
+        {
+            get { return GetAttr("label"); }
+            set { SetAttr("label", value); }
+        }
+
+        public string Value
+        {
+            get { return GetAttr("value"); }
+            set { SetAttr("value", value); }
+        }
+
+        public string ValueFormat
+        {
+            get { return GetAttr("valueFormat"); }
+            set { SetAttr("valueFormat", value); }
+        }
+
+        public bool ShowValue
+        {
+            get { return GetBoolAttr("showValue"); }
+            set { SetBoolAttr("showValue", value); }
+        }
+
+        public bool Vertical
+        {
+            get { return GetBoolAttr("vertical"); }
+            set { SetBoolAttr("vertical", value); }
+        }
+
+        public float Min
+        {
+            get { return GetAttr<float>("min"); }
+            set { SetAttr("min", value); }
+        }
+
+        public float Max
+        {
+            get { return GetAttr<float>("max"); }
+            set { SetAttr("max", value); }
+        }
+
+        public float Step
+        {
+            get { return GetAttr<float>("step"); }
+            set { SetAttr("step", value); }
+        }
+
+        public EventHandler OnChange
+        {
+            get { return GetEventHandler("change"); }
+            set
+            {
+                SetEventHandler("change", value);
+                SetBoolAttr("onchange", value != null);
+            }
+        }
+    }
+}
---
+++ src/Pglet/Controls/SpinButton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Pglet.Controls
+{
+    public class SpinButton : Control
+    {
+        protected override string ControlName => "spinbutton";
+
+        public string Label
+        {
+            get { return GetAttr("label"); }
+            set { SetAttr("label", value); }
+        }
+
+        public string Value
+        {
+            get { return GetAttr("value"); }
+            set { SetAttr("value", value); }
+        }
+
+        public string Icon
+        {
+            get { return GetAttr("icon"); }
+            set { SetAttr("icon", value); }
+        }
+
+        public float Min
+        {
+            get { return GetAttr<float>("min"); }
+            set { SetAttr("min", value); }
+        }
+
+        public float Max
+        {
+            get { return GetAttr<float>("max"); }
+            set { SetAttr("max", value); }
+        }
+
+        public float Step
+        {
+            get { return GetAttr<float>("step"); }
+            set { SetAttr("step", value); }
+        }
+
+        public EventHandler OnChange
+        {
+            get { return GetEventHandler("change"); }
+            set
+            {
+                SetEventHandler("change", value);
+                SetBoolAttr("onchange", value != null);
+            }
+        }
+    }
+}
