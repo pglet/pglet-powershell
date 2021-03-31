aaad657765d213d6aa16592a0086a1a51bb36f87
Feodor Fitsner <feodor@appveyor.com>
2021-03-31 09:39:34 -0700

    Progress

---
+++ src/Pglet/Controls/Progress.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Pglet.Controls
+{
+    public class Progress : Control
+    {
+        protected override string ControlName => "progress";
+
+        public int Value
+        {
+            get { return GetIntAttr("value"); }
+            set { SetIntAttr("value", value); }
+        }
+
+        public string Label
+        {
+            get { return GetAttr("label"); }
+            set { SetAttr("label", value); }
+        }
+
+        public string Description
+        {
+            get { return GetAttr("description"); }
+            set { SetAttr("description", value); }
+        }
+    }
+}
