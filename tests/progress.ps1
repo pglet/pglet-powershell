ab8ee7e0a348747ae8e8454313f3b4f4bc86d2b2
Feodor Fitsner <feodor@appveyor.com>
2021-08-04 16:59:30 -0700

    Permissions

--- tests/progress.ps1
+++ tests/progress.ps1
@@ -1,7 +1,7 @@
 Remove-Module pglet -ErrorAction SilentlyContinue
 Import-Module ([IO.Path]::Combine((get-item $PSScriptRoot).parent.FullName, 'pglet.psd1'))
 
-Connect-PgletApp -Name "pglet-progress" -Local -ScriptBlock {
+Connect-PgletApp -Name "pglet-progress" -ScriptBlock {
   $ErrorActionPreference = 'stop'
 
   $page = $pglet_page
